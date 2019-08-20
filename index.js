f1af4fe7a71637cddbcee822804abd045f52615b
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-20 18:38:44 +0900

    [update]簡単な関数を追加

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function returnIfGreaterThan0(num) {
+    if (num > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = returnIfGreaterThan0;
